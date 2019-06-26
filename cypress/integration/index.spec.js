7e621e88e970be6a5553bfcea2eaa69632f93b15
Jodel Asur <jodelasur@gmail.com>
2019-06-26 13:21:23 +0800

    Update index.spec.js

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,7 +1,25 @@
 describe('Index', () => {
-  it('goes to new.html after pressing Enter', () => {
+  it('goes to new.html after {enter} + trigger input', () => {
     cy.visit('app/index.html');
     cy.get('#id_text').type('typing{enter}').trigger('input');
+    cy.contains('new.html here');
+  });
+  
+  it('goes to new.html after {enter}', () => {
+    cy.visit('app/index.html');
+    cy.get('#id_text').type('typing{enter}');
+    cy.contains('new.html here');
+  });
+  
+  it('goes to new.html after separate {enter}', () => {
+    cy.visit('app/index.html');
+    cy.get('#id_text').type('typing').type('{enter}');
+    cy.contains('new.html here');
+  });
+  
+  it('goes to new.html after separate {enter} + trigger input', () => {
+    cy.visit('app/index.html');
+    cy.get('#id_text').type('typing').type('{enter}').trigger('input');
     cy.contains('new.html here');
   });
 });
